9af8c7f13bfa4e33478117eedda2c0657bb949bf
Dawuti MAIHEMUTIJIANG <0600004923@sanoh.com>
2023-10-23 17:39:16 +0900

    modeified onboardingScreen

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -11,11 +11,54 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          width: MediaQuery.of(context).size.width * 0.95,
-          height: MediaQuery.of(context).size.height *0.85,
-          color: Colors.black,
+      body: SafeArea(
+        child: PageView.builder(
+          itemCount: 3,
+          itemBuilder: (context, index){
+            return Center(
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.95,
+                height: MediaQuery.of(context).size.height *0.85,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  gradient: const LinearGradient(
+                    colors: [Colors.black, Colors.grey],
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft,
+                  ),
+                  borderRadius: BorderRadius.circular(35),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset('assets/Bell Peppers.png'),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    const Text("Bell Peppers", style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 36,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 15.0),
+                      child: Text(
+                        "Some Information about the vegetable. Some Information about the vegetable. Some Information about the vegetable",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
         ),
       ),
     );
