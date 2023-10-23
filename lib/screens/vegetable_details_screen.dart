13e629ee9b18aaea568f4b716fa58f007df4786b
Dawuti MAIHEMUTIJIANG <0600004923@sanoh.com>
2023-10-23 17:05:38 +0900

    first commit

---
+++ lib/screens/vegetable_details_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class VegetableDetailsScreen extends StatelessWidget {
+  const VegetableDetailsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('VegetableDetailsScreen'),
+      ),
+    );
+  }
+}
